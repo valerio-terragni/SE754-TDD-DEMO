d2bfd7296a9dfbdb3c797aec59b76fd65002526b
Valerio Terragni <v.terragni@auckland.ac.nz>
2022-04-11 22:21:58 +0800

    write enough code to make the test pass

--- src/main/java/nz/ac/auckland/se754/Max.java
+++ src/main/java/nz/ac/auckland/se754/Max.java
@@ -3,7 +3,7 @@ package nz.ac.auckland.se754;
 public class Max {
 
     public static int getMax(int num1, int num2){
-        return 0;
+        return num2;
     }
 
 }
--- src/test/java/nz/ac/auckland/se754/MaxTest.java
+++ src/test/java/nz/ac/auckland/se754/MaxTest.java
@@ -2,12 +2,15 @@ package nz.ac.auckland.se754;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MaxTest {
 
     @Test
     public void test_max_should_return_20(){
         int expected = 20;
         int actual = Max.getMax(10,20);
+        assertEquals(expected, actual);
     }
 
 }
