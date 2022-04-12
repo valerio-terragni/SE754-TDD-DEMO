319af5698a69f997c4ce4544a0759cfeb8861902
Valerio Terragni <v.terragni@auckland.ac.nz>
2022-04-12 09:11:11 +0800

    add PasswordChecker and User to make the test compile

---
+++ src/main/java/nz/ac/auckland/se754/PasswordChecker.java
@@ -0,0 +1,10 @@
+package nz.ac.auckland.se754;
+
+public class PasswordChecker {
+
+    public PasswordChecker(User user) {
+    }
+      public boolean checkPasswordAndSet(String pwd){
+          return false;
+        }
+}
---
+++ src/main/java/nz/ac/auckland/se754/User.java
@@ -0,0 +1,4 @@
+package nz.ac.auckland.se754;
+
+public interface User {
+}
